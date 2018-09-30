c5afe484bcf73796cee0ecf48e7b43a44a39b24c
Sangbum Kim <bearksb1115@gmail.com>
2018-09-30 23:36:51 +0900

    Create molar_mass.py

---
+++ template/molar_mass.py
@@ -0,0 +1,17 @@
+import csv
+
+def table_to_dict(data_file):
+    ## IMPLEMENT HERE! ##
+
+
+def parse_element(element_str):
+    ## IMPLEMENT HERE! ##
+
+# Open file
+data_file = open('../Periodic-Table.csv', encoding='windows-1252')
+
+# Create dictionary
+periodic_dict = table_to_dict(data_file)
+
+# Get input and parse
+## IMPLEMENT HERE! ##
