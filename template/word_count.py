1f8443aedd70899bd5004b8a844e7334d91a0c05
Sangbum Kim <bearksb1115@gmail.com>
2018-09-30 21:30:44 +0900

    Update word_count.py
    fix path

--- template/word_count.py
+++ template/word_count.py
@@ -10,4 +10,4 @@ def word_count(file_path):
 test_files = ['gettysburg.txt', 'shark.txt']
 for file_name in test_files:
     print('=== Word count for {:s} ==='.format(file_name))
-    word_count(file_name)
+    word_count('../' + file_name)
